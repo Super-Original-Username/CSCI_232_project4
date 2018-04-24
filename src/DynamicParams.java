628801c6484c8782641caa07c321bb2424981411
aswordlessninja <aswordlessninja@gmail.com>
2018-04-24 08:56:00 -0600

    I added more comments

--- src/DynamicParams.java
+++ src/DynamicParams.java
@@ -1,9 +1,18 @@
 /*
- * Authors: Ethan Fison (aswordlessninja on github), Alex Salois (asalois), Zan Rost-Montieth (Zan-RostMontieth)
+ * Authors:(gitHub username in parentheses) Ethan Fison (aswordlessninja), Alex Salois (asalois), Zan Rost-Montieth (Zan-RostMontieth)
  * Date: 4/23/18
  * Overview: Dynamic change-making algorithm. Also uses jUnit for tests to make sure it works proper-like.
  * Modified version of the greedy change-making algorithm from lab 7
- * Uses JUNIT 5 for all tests
+ * Uses JUnit 5 for all tests
+ *
+ * How to use: Run JUnit tests as usual. If you want to change the init ("in" in the test class) value being entered, do
+ * so from the seeIfItWorks test in tests.java. You'll also need to edit the items added to the arrayList, as those are
+ * used in the comparison when running the test. If you only want to check the output of the program, don't worry about
+ * the arrayList, you should just be able to edit the "in" variable in that test, and JUnit should spit out the error,
+ * with both the expected and actual outputs
+ *
+ * Sorry if most of the commits are from me (Ethan). There wasn't really a whole lot to change from lab 7, so I ended up
+ * doing the majority of the commits when we met up to work on this.
  */
 
 import java.util.ArrayList;
@@ -60,19 +69,14 @@ public class DynamicParams {
                 }
             }
         }
+        // This section iterates through the totalCoins array. It starts at the last value in the array (init), adds
+        // that to the arrayList, prints it to the console, then changes the current index to the current - the coin value
         int temp = init;
         while(temp > 0){
             int thisCoin = totalCoins[temp];
             coinsUsed.add(thisCoin);
+            System.out.println(thisCoin);
             temp = temp - thisCoin;
-        }
-
-        // Iterates through the arraylist to print the values
-        for (int j = 0; j < coinsUsed.size(); j++) {
-            if (j == coinsUsed.size() - 1) {
-                System.out.println(coinsUsed.get(j));
-            } else
-                System.out.print(coinsUsed.get(j) + ", ");
         }
         return coinsUsed;
     }
